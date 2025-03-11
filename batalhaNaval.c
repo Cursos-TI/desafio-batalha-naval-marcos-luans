d29cbb316506972275dcbbbb9ea0fd8c8b3574b4
marcos-luans <marcosluanscalisto@gmail.com>
2025-03-11 23:09:24 +0000

    Tabuleiro completo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -12,6 +12,42 @@ void inicializarTabuleiro(int tabuleiro[TAMANHO][TAMANHO]) {
     }
 }
 
+// Verifica se um navio pode ser posicionado sem sobreposição e dentro dos limites
+int podePosicionar(int tabuleiro[TAMANHO][TAMANHO], int linha, int coluna, char direcao) {
+    for (int i = 0; i < NAVIO; i++) {
+        if (direcao == 'V' && (linha + i >= TAMANHO || tabuleiro[linha + i][coluna] != 0)) {
+            return 0;
+        }
+        if (direcao == 'H' && (coluna + i >= TAMANHO || tabuleiro[linha][coluna + i] != 0)) {
+            return 0;
+        }
+        if (direcao == 'D' && (linha + i >= TAMANHO || coluna + i >= TAMANHO || tabuleiro[linha + i][coluna + i] != 0)) {
+            return 0;
+        }
+        if (direcao == 'D2' && (linha + i >= TAMANHO || coluna - i < 0 || tabuleiro[linha + i][coluna - i] != 0)) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+// Posiciona um navio no tabuleiro
+void posicionarNavio(int tabuleiro[TAMANHO][TAMANHO], int linha, int coluna, char direcao) {
+    if (podePosicionar(tabuleiro, linha, coluna, direcao)) {
+        for (int i = 0; i < NAVIO; i++) {
+            if (direcao == 'V') {
+                tabuleiro[linha + i][coluna] = 3;
+            } else if (direcao == 'H') {
+                tabuleiro[linha][coluna + i] = 3;
+            } else if (direcao == 'D') {
+                tabuleiro[linha + i][coluna + i] = 3;
+            } else if (direcao == 'D2') {
+                tabuleiro[linha + i][coluna - i] = 3;
+            }
+        }
+    }
+}
+
 // Exibe o tabuleiro com identificação de linhas e colunas
 void exibirTabuleiro(int tabuleiro[TAMANHO][TAMANHO]) {
     printf("   ");
@@ -29,17 +65,6 @@ void exibirTabuleiro(int tabuleiro[TAMANHO][TAMANHO]) {
     }
 }
 
-// Posiciona um navio no tabuleiro
-void posicionarNavio(int tabuleiro[TAMANHO][TAMANHO], int linha, int coluna, char direcao) {
-    for (int i = 0; i < NAVIO; i++) {
-        if (direcao == 'V') {
-            tabuleiro[linha + i][coluna] = 3;
-        } else if (direcao == 'H') {
-            tabuleiro[linha][coluna + i] = 3;
-        }
-    }
-}
-
 int main() {
     int tabuleiro[TAMANHO][TAMANHO];
 
@@ -48,6 +73,8 @@ int main() {
     // Posicionamento fixo dos navios
     posicionarNavio(tabuleiro, 1, 2, 'V'); // Navio vertical
     posicionarNavio(tabuleiro, 6, 4, 'H'); // Navio horizontal
+    posicionarNavio(tabuleiro, 3, 3, 'D'); // Navio diagonal principal
+    posicionarNavio(tabuleiro, 2, 8, 'D2'); // Navio diagonal secundária
 
     exibirTabuleiro(tabuleiro);
     return 0;
